9ede4677cc04eb8c9c1ca3f507c2547f5e1ef09d
Zuzanna Zak <5zak@taurus.fis.agh.edu.pl>
2017-11-15 18:50:14 +0100

    message

--- kol2.py
+++ kol2.py
@@ -13,3 +13,52 @@
 # If you have enough courage and time, try storing (reading/writing) 
 # data in text files (YAML, JSON).
 # If you have even more courage, try implementing user interface.
+
+#!usr/bin/python
+
+class Subject:
+	def __init__(self):
+		self.scores=[]
+	
+	def add (self, score):
+		self.scores.append(score)
+
+
+class Student:
+	def __init__(self, name, surname):
+		self.name=name
+		self.surname=surname
+		self.classes=[]
+
+	def add_class (self, class_name):
+		
+		self.classes.append(class_name)
+	
+	def add_score(self, class_name, score):
+		index = self.classes.index(class_name)
+		self.classes[index].add(score)
+
+	def get_average_in_class(self, class_name):
+		index = self.classes.index(class_name)
+		#average=sum(self.classes[index].scores)/len(self.classes[index].scores
+		#return sum(self.classes[index].scores)/len(self.classes[index].scores
+		
+
+		
+
+
+
+
+
+
+if __name__=="__main__":
+
+	maths=Subject()
+	s1=Student("Jan", "Kowalski")
+	s1.add_class(maths)
+	s1.add_score(maths, 5)
+	#print(s1.get_average_in_class(maths))
+	
+	
+
+		
