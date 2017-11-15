6d4d8df5e25114e9ea5d0cb8c8e6f4946072c4e1
mwmajew <maciej.w.majewski@gmail.com>
2017-11-15 17:47:50 +0100

    Create kol2.py

---
+++ kol2.py
@@ -0,0 +1,15 @@
+# Class diary  
+#
+# Create program for handling lesson scores.
+# Use python to handle student (highscool) class scores, and attendance.
+# Make it possible to:
+# - Get students total average score (average across classes)
+# - get students average score in class
+# - hold students name and surname
+# - Count total attendance of student
+# The default interface for interaction should be python interpreter.
+# Please, use your imagination and create more functionalities. 
+# Your project should be able to handle entire school.
+# If you have enough courage and time, try storing (reading/writing) 
+# data in text files (YAML, JSON).
+# If you have even more courage, try implementing user interface.
